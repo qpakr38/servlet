50df95f6ac2d8b5d3c310126b963d17c282aa3d3
qpakr38 <zczc0819@gmail.com>
2022-04-06 19:27:22 +0900

    FEAT : FrontController 도입

---
+++ src/main/java/hello/servlet/web/froncontroller/MyView.java
@@ -0,0 +1,32 @@
+package hello.servlet.web.froncontroller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+import java.io.IOException;
+import java.util.Map;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequstAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request,response);
+    }
+
+    private void modelToRequstAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value)-> request.setAttribute(key,value));
+    }
+}
