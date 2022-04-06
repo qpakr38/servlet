be11bcb4ef4189b4be86f2353f69e8ba5baf540b
qpakr38 <zczc0819@gmail.com>
2022-04-06 19:28:33 +0900

    REAFATOR : V4

---
+++ src/main/java/hello/servlet/web/froncontroller/v4/controller/MemberFormControllerV4.java
@@ -0,0 +1,15 @@
+package hello.servlet.web.froncontroller.v4.controller;
+
+import hello.servlet.web.froncontroller.ModelView;
+import hello.servlet.web.froncontroller.v4.ControllerV4;
+
+
+import java.util.Map;
+
+public class MemberFormControllerV4 implements ControllerV4 {
+
+    @Override
+    public String process(Map<String, String> paramMap,Map<String,Object> model) {
+        return "new-form";
+    }
+}
