e51060f8c44f6cab1b96b89a287a37bab75b139d
qpakr38 <zczc0819@gmail.com>
2022-04-06 19:28:07 +0900

    REAFATOR : V2

---
+++ src/main/java/hello/servlet/web/froncontroller/v2/ControllerV2.java
@@ -0,0 +1,12 @@
+package hello.servlet.web.froncontroller.v2;
+
+import hello.servlet.web.froncontroller.MyView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface ControllerV2 {
+    MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
